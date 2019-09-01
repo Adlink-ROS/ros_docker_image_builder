1b48767626e8a216191cc39b5b200b7ff263942c
Skyler Pan <skylerpan@gmail.com>
2019-09-02 00:43:52 +0800

    [ROS2] Support ROS2 dashing.

--- build_ros.sh
+++ build_ros.sh
@@ -3,9 +3,15 @@
 case $1 in
 	kinetic)
 		DOCKER_FILE=ros_kk
+		IMAGE_NAME=ros1_$1
 	;;
 	melodic)
 		DOCKER_FILE=ros_mm
+		IMAGE_NAME=ros1_$1
+	;;
+	dashing)
+		DOCKER_FILE=ros2_dd
+		IMAGE_NAME=ros2_$1
 	;;
 	*)
 		if [ -z $1 ]; then
@@ -21,12 +27,12 @@ echo "Building $1"
 
 sudo xhost local:root
 sudo xhost +local:root
-sudo docker build -t ros_$1 . --no-cache -f $DOCKER_FILE
+sudo docker build -t $IMAGE_NAME . --no-cache -f $DOCKER_FILE
 sudo docker run -itd \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --name "$1" \
-    ros_$1
+    $IMAGE_NAME
 export containerId=$(sudo docker ps -l -q)
 
